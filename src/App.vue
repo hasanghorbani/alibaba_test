eb0b0c30dd377fd56ea0e90bb41d3909294cb6ac
Hassan <hassanghorbani>
2023-04-04 10:38:18 +0330

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const change_theme = () => {
+	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+		const newColorScheme = event.matches ? "dark" : "light";
+		console.log(newColorScheme)
+	});
+}
+
+</script>
+
+<template>
+  <header>
+   <h1>Where in the world?</h1>
+
+    <div @click="change_theme">
+<!--	    <ion-icon name="moon-outline"></ion-icon>-->
+	    <ion-icon name="moon"></ion-icon>
+	    Dark Mode
+    </div>
+  </header>
+
+  <main>
+  
+  </main>
+</template>
+
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+  line-height: 1.5;
+	padding: 1rem;
+}
+
+@media (min-width: 1024px) {
+
+}
+</style>
