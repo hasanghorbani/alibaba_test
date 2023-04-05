305a1adbfb1eb8497b7350d3abbad872478a5985
Hassan <hassanghorbani>
2023-04-05 11:14:26 +0330

    feat: add detail

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -1,40 +1,44 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 defineProps<{
 	id: string
 }>()
 </script>
 <template>
 	<section class="container flex justify-between">
-		<router-link to="/" class="shadow flex items-center back">
+		<router-link class="shadow flex items-center back" to="/">
 			<ion-icon name="arrow-back"></ion-icon>
 			Back
 		</router-link>
 	</section>
-	<section class="container flex ">
-		<div class="flag">
-			<img src=""/>
-		</div>
-		<div>
-			<h1>
-				NAme
-			</h1>
-			
-			<div>
-				<ul class="w-50">
-					<li></li>
-				</ul>
-				
-				<ul  class="w-50">
-					<li></li>
-				</ul>
+	<section class="container flex items-start">
+			<div class="flag w-50 ">
+				<img src="https://flagcdn.com/bb.svg" class="w-100"/>
 			</div>
+			<div class="w-50 detail">
+				<h1>
+					NAme
+				</h1>
+				
+				<div class="flex">
+					<ul class="w-50">
+						<li><strong>Population:</strong><span> </span></li>
+						<li><strong>Region:</strong><span> </span></li>
+						<li><strong>Capital:</strong><span> </span></li>
+					</ul>
+					
+					<ul>
+						<li><strong>Population:</strong><span></span></li>
+						<li><strong>Region:</strong><span> </span></li>
+						<li><strong>Capital:</strong><span>  </span></li>
+					</ul>
+				</div>
 		</div>
 	</section>
-	
+
 </template>
 <style scoped>
 
-.back{
+.back {
 	background: var(--color-element);
 	padding: 10px 32px;
 	font-size: .9rem;
@@ -44,9 +48,20 @@ defineProps<{
 	color: var(--color-text);
 	border-radius: 7px;
 }
-button ion-icon{
+
+button ion-icon {
 	margin-right: 5px;
 }
-
-
+.flag{
+	padding: 0 64px;
+}
+.detail {
+	padding: 64px;
+}
+.detail ul{
+	line-height: 2;
+}
+.detail h1{
+	margin-bottom: 64px;
+}
 </style>
