770e53b851b43d8f2becf212d3bf24ccd7c93707
Hassan <hassanghorbani>
2023-04-05 11:21:54 +0330

    add border countries list

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -11,27 +11,36 @@ defineProps<{
 		</router-link>
 	</section>
 	<section class="container flex items-start">
-			<div class="flag w-50 ">
-				<img src="https://flagcdn.com/bb.svg" class="w-100"/>
-			</div>
-			<div class="w-50 detail">
-				<h1>
-					NAme
-				</h1>
+		<div class="flag w-50 ">
+			<img class="w-100" src="https://flagcdn.com/bb.svg"/>
+		</div>
+		<div class="w-50 detail">
+			<h1>
+				NAme
+			</h1>
+			
+			<div class="flex">
+				<ul class="w-50">
+					<li><strong>Population:</strong><span> </span></li>
+					<li><strong>Region:</strong><span> </span></li>
+					<li><strong>Capital:</strong><span> </span></li>
+				</ul>
 				
-				<div class="flex">
-					<ul class="w-50">
-						<li><strong>Population:</strong><span> </span></li>
-						<li><strong>Region:</strong><span> </span></li>
-						<li><strong>Capital:</strong><span> </span></li>
-					</ul>
-					
-					<ul>
-						<li><strong>Population:</strong><span></span></li>
-						<li><strong>Region:</strong><span> </span></li>
-						<li><strong>Capital:</strong><span>  </span></li>
-					</ul>
+				<ul>
+					<li><strong>Population:</strong><span></span></li>
+					<li><strong>Region:</strong><span> </span></li>
+					<li><strong>Capital:</strong><span>  </span></li>
+				</ul>
+			</div>
+			
+			<div class="flex items-center">
+				<h2>Border Countries:</h2>
+				<div class="border_buttons">
+					<router-link class="shadow flex items-center back" to="/">
+						BLC
+					</router-link>
 				</div>
+			</div>
 		</div>
 	</section>
 
@@ -52,16 +61,24 @@ defineProps<{
 button ion-icon {
 	margin-right: 5px;
 }
-.flag{
-	padding: 0 64px;
-}
+
 .detail {
 	padding: 64px;
 }
-.detail ul{
+
+.detail ul {
 	line-height: 2;
 }
-.detail h1{
+
+.detail h1 {
 	margin-bottom: 64px;
+}
+
+.border_buttons {
+	margin-left: 10px;
+}
+
+.border_buttons a {
+	padding: 5px 16px;
 }
 </style>
